e949fb9c27ee93a3a8ebd6d934ad15f6ac54066a
vishalkwatra <vishal.kwatra88@gmail.com>
2020-09-13 20:22:34 +0530

    One controller called home controller and one route

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running the server: ${err}`);
     }
 
-    console.log(`Server is running on port:${port + 1}`);
+    console.log(`Server is running on port:${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controller/home_controller');
+
+console.log('Router Loaded');
+
+router.get('/',homeController.home);
+
+
+module.exports = router;
