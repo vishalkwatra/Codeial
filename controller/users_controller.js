38fb5508fc2c98caa75f58a3facd5f69f8290716
vishalkwatra <vishal.kwatra88@gmail.com>
2020-11-05 11:39:12 +0530

    Sign out and Sign in links

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -16,7 +16,7 @@ module.exports.signUp = function (req, res) {
     if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
-    
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     });
@@ -58,5 +58,10 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function (req, res) {
+    req.logout();   // given by passport
     return res.redirect('/');
 }
