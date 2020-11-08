bc507c28345337ea445920e4bbccc688a585eccd
vishalkwatra <vishal.kwatra88@gmail.com>
2020-11-08 14:25:45 +0530

    User population for the posts

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,13 +1,29 @@
-module.exports.home = function(req,res){
+const Post = require('../models/post');
+module.exports.home = function (req, res) {
     console.log(req.cookies);
-//    res.cookie('user_id',25);
+    // Post.find({
+    //     user: req.user._id 
+    // },function(err, posts){
+    //     return res.render('home', {
+    //         posts: posts,
+    //         title: "Home"
+    //     });    
+    // });
+
+    Post.find({}).populate('user').exec(function (err, posts) {
+        return res.render('home', {
+            posts: posts,
+            title: "Home"
+        });
+    }
+    );
+
+    //    res.cookie('user_id',25);
     // return res.end('<h1>Express is up for Codeial</h1>');
-    return res.render('home', {
-        title: "Home"
-    });
+
 }
 
-module.exports.second_home = function(req,res){
+module.exports.second_home = function (req, res) {
     return res.send('<h1>Second home action</h1>');
 }
 
