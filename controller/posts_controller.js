46bf2156c83a4c18d6b507a0dd810e45352a9322
vishalkwatra <vishal.kwatra88@gmail.com>
2020-11-13 14:37:58 +0530

    deleting the post along with comments

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = (req, res) => {
     Post.create({
@@ -10,6 +11,22 @@ module.exports.create = (req, res) => {
             return;
         }
         return res.redirect('back');
+    });
+}
+
+
+module.exports.destroy = function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({ post: req.params.id }, function (err) {
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
     });
 }
 
