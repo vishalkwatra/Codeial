68cc9ee6ac4480ad214add76c9f866e3395d8595
vishalkwatra <vishal.kwatra88@gmail.com>
2020-11-18 19:34:25 +0530

    Post creation via AJAX

---
+++ assets/js/home_posts.js
@@ -0,0 +1,54 @@
+{
+    // method to submit form for creation of post via AJAX
+    let createPost = function () {
+        let newPostForm = $('#new-post-form');
+
+        newPostForm.submit(function (e) {
+            console.log('test');
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function (data) {
+                    // console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
+                },
+                error: function (error) {
+                    console.log(error.responseText);
+                }
+            });
+        });
+    }
+
+    // method to create a post in DOM
+    let newPostDom = function (post) {
+        return $(`<li id="post-${post._id}">
+                        <p>
+                            <small>
+                                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+                            </small>
+                            ${post.content}
+                            <br>
+                            <small>
+                            ${post.user.name}   
+                            </small>
+                        </p>
+                        <div class="post-comments">
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment">
+                                <input type="hidden" name="post" value="${post._id}">
+                                <input type="submit" value="Add Comment">
+                            </form>
+                    
+                            <div class="post-comments-list">
+                                <ul id="post-comments-${post._id}">
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`);
+    }
+
+    createPost();
+}
