3bf5b32808d0926d892ccf8e769df01776e63875
vishalkwatra <vishal.kwatra88@gmail.com>
2020-11-23 16:41:35 +0530

    Show the avatar on the profile page

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+// make the upload path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 //extract style and scripts from subpages into the layout
