486132d3739f474f441f16803114020bcf2535e0
vishalkwatra <vishal.kwatra88@gmail.com>
2020-11-27 14:27:57 +0530

    API for getting the posts and deleting the posts

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const postsApi = require('../../../controller/api/v1/posts_api');
 
 router.get('/', postsApi.index);
-
+router.delete('/:id', postsApi.destroy);
 
 module.exports = router;
