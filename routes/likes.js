e6e531602c056b31d50345b1fc2c50157f59a0d1
vishalkwatra <vishal.kwatra88@gmail.com>
2020-12-12 14:03:22 +0530

    Likes Scehma

---
+++ routes/likes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const likesController = require('../controller/likes_controller');
+
+router.post('/toggle', likesController.toggleLike);
+
+module.exports = router;
